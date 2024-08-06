1dbc2bbf9cb9a88336c90749468257ae99f73677
yunapiyo <vocalolove130919@gmail.com>
2024-08-07 00:20:48 +0900

    kadai_020を再再々プッシュ

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,11 +1,15 @@
-//html要素を取得し、中身を出力する
-console.log(window);
+// textというidを持つ要素を定数textElementに代入
+const textElement = document.getElementById('text');
+// btnというid名を持つ要素を定数btnElementに代入
+const btnElement = document.getElementById('btn');
+
 
 //textというidを持つ要素を取得し、中身を出力する
 console.log(document.getElementById('text'));
 
 //btnというid名を持つhtml要素を取得し、中身を出力する
 console.log(document.getElementById('btn'));
+
 
 //ボタンがクリックされた時に実行されるイベントリスナーを追加
 btn.addEventListener('click',() => {
