6a0ceadf73e29606f8822a49026329446c8e668f
yunapiyo <vocalolove130919@gmail.com>
2024-08-08 22:02:18 +0900

    kadai_021をプッシュ

---
+++ kadai_021/event.js
@@ -0,0 +1,19 @@
+// textというidを持つ要素を定数textElementに代入
+const textElement = document.getElementById('text');
+// btnというid名を持つ要素を定数btnElementに代入
+const btnElement = document.getElementById('btn');
+
+
+//textというidを持つ要素を取得し、中身を出力する
+console.log(document.getElementById('text'));
+
+//btnというid名を持つhtml要素を取得し、中身を出力する
+console.log(document.getElementById('btn'));
+
+//ボタンがクリックされた時に実行されるイベントリスナーを追加
+btn.addEventListener('click',() => {
+  //2秒後に見出しのテキストを変更
+ setTimeout(() => {
+   textElement.textContent = 'ボタンをクリックしました';
+ },2000);
+});
