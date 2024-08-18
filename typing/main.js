8757bd27a2aea740db69d9581305966107c3501e
yunapiyo <vocalolove130919@gmail.com>
2024-08-18 18:26:06 +0900

    typingを再プッシュ

--- typing/main.js
+++ typing/main.js
@@ -1,12 +1,14 @@
 //変数の初期化
 let untyped = '';
 let typed = '';
+let score = 0;
 
 //必要なhtml要素の取得
 const untypedfield = document.getElementById('untyped');
 const typedfield = document.getElementById('typed');
 const wrap = document.getElementById('wrap');
-
+const start = document.getElementById('start');
+const count = document.getElementById('count');
 
 //複数のテキストを格納する配列
 const textLists = [
@@ -40,38 +42,104 @@ const createText = () => {
   untypedfield.textContent = untyped;
 };
 
-createText();
  
 // キー入力の判定
 const keyPress = e => {
 
-//誤タイプの場合
-if(e.key !== untyped.substring(0,1)) {
-  wrap.classList.add('mistyped');
-  return;
-}
+  //誤タイプの場合
+  if(e.key !== untyped.substring(0,1)) {
+    wrap.classList.add('mistyped');
+    //100ms後に背景色を元に戻す
+    setTimeout(() => {
+      wrap.classList.remove('mistyped');
+    },100);
+    return;
+  }
 
   //正タイプの場合
-  wrap.classList.remove('mistyped');
-  typed += untyped.substring(0,1);
-  untyped = untyped.substring(1);
-  typedfield.textContent = typed;
-  untypedfield.textContent = untyped;
+  // スコアのインクリメント
+    score++;
+    wrap.classList.remove('mistyped');
+    typed += untyped.substring(0,1);
+    untyped = untyped.substring(1);
 
- //テキストがなくなったら新しいテキストを表示
- if(untyped === '') {
-  createText();
- }
+
+  // 正しい入力部分を<span>タグで囲んでクラスを追加
+    typedfield.innerHTML = `<span class="typed">${typed}</span>`;
+    untypedfield.textContent = untyped;
+
+  //テキストがなくなったら新しいテキストを表示
+  if(untyped === '') {
+    createText();
+  }
 };
 
 // タイピングスキルのランクを判定
-const rankCheck = score => {};
+const rankCheck = score => {
+
+  // テキストを格納する変数を作る
+  let text = '';
+  
+  // スコアに応じて異なるメッセージを変数textに格納する
+  if(score < 100) {
+   text = `あなたのランクはCです。\nBランクまであと${100 - score}文字です。`;
+  }else if(score < 200) {
+    text = `あなたのランクはBです。\nAランクまであと${200 - score}文字です。`;    
+  } else if(score < 300) {
+    text = `あなたのランクはAです。\nSランクまであと${300 - score}文字です。`;    
+  } else if(score >= 300) {
+    text = `あなたのランクはSです。\nおめでとうございます!`;    
+  }
+  
+  // 生成したメッセージと一緒に文字列を返す
+  return `${score}文字打てました!\n${text}\n【OK】リトライ / 【キャンセル】終了`;
+};
 
 // ゲームを終了
-const gameOver = id => {};
+const gameOver = id => {
+  clearInterval(id);
+ 
+  const result = confirm(rankCheck(score));
+
+  // OKボタンをクリックされたらリロードする
+  if(result == true) {
+    window.location.reload();
+  }
+};
 
 // カウントダウンタイマー
-const timer = () => {};
+const timer = () => {
 
-//キーボードのイベント処理
-document.addEventListener('keypress',keyPress);
+  //タイマー部分のhtml要素(p要素)を取得する
+  let time = count.textContent;
+
+  const id = setInterval(() => {
+
+    //カウントダウンする
+    time--;
+    count.textContent = time;
+
+    //カウントが0になったらタイマーを停止する
+    if(time <= 0) {
+     gameOver(id);
+    }
+  },1000);
+};
+
+//ゲームスタート時の処理
+start.addEventListener('click',() => {
+
+  //カウントダウンタイマーを開始する
+  timer();
+
+  //ランダムなテキストを表示する
+  createText();
+
+  //「スタート」ボタンを非表示にする
+  start.style.display = 'none';
+
+  //キーボードのイベントの処理
+  document.addEventListener('keypress',keyPress);
+});
+
+untypedfield.textContent = 'スタートボタンで開始';
